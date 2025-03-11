13c78222f7e30b9a726b93e2dc5ad80b297b9a12
Sigit-git <2311102441050@umkt.ac.id>
2025-03-11 23:30:37 +0800

    update file urls.py

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import home, about
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('about/', about, name='about')
 ]
